ca40498c876ea0b9fd23df28a571d9aa46de4023
Nayaka Al Syahreal K <traitorget@gmail.com>
2022-08-09 03:31:48 +0700

    Update Index
    Make Index

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <>
+      <div className="flex w-64 h-40">
+        <div className="shrink h-max w-max bg-sky-900">
+            <h1>Test</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
